14bcdc7e6f7e6723dad13e4ebbdf5a8c4630ea61
pngwn <hello@pngwn.io>
2019-04-22 15:25:17 +0100

    init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+
+<main>
+	<slot></slot>
+</main>
